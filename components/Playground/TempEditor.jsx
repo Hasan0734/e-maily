9b8896a83151c00854894fb463d3e6051a906495
Hasan0734 <jahidhasan2856@gmail.com>
2025-03-19 08:49:05 +0600

    added email builder-js

--- components/Playground/TempEditor.jsx
+++ components/Playground/TempEditor.jsx
@@ -4,6 +4,8 @@ import { Card } from "../ui/card";
 import EmailEditor from "react-email-editor";
 import { Button } from "../ui/button";
 import sample from "./sample.json";
+import { Reader } from '@usewaypoint/email-builder';
+
 
 const TempEditor = ({ activeTab, setTemplate }) => {
   const emailEditorRef = useRef(null);
@@ -51,6 +53,35 @@ const TempEditor = ({ activeTab, setTemplate }) => {
     console.log("onLoad", unlayer);
     unlayer.loadDesign(sample);
   };
+  const CONFIGURATION = {
+    root: {
+      type: 'EmailLayout',
+      data: {
+        backdropColor: '#F8F8F8',
+        canvasColor: '#FFFFFF',
+        textColor: '#242424',
+        fontFamily: 'MODERN_SANS',
+        childrenIds: ['block-1709578146127'],
+      },
+    },
+    'block-1709578146127': {
+      type: 'Text',
+      data: {
+        style: {
+          fontWeight: 'normal',
+          padding: {
+            top: 16,
+            bottom: 16,
+            right: 24,
+            left: 24,
+          },
+        },
+        props: {
+          text: 'Hello world',
+        },
+      },
+    },
+  };
 
   return (
     <TabsContent value="design" className="">
@@ -58,12 +89,13 @@ const TempEditor = ({ activeTab, setTemplate }) => {
         <Button onClick={saveDesign}>Save</Button>
       </div>
 
-      <EmailEditor
+     <Reader document={CONFIGURATION} rootBlockId="root" />;
+      {/* <EmailEditor
         id="editor-container"
         ref={emailEditorRef}
         onReady={onReady}
         onLoad={onLoad}
-      />
+      /> */}
     </TabsContent>
   );
 };
